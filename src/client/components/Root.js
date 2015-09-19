719d360da938d6b6b9ee46b01acb6ef575a1c978
broucz <broucapierre@gmail.com>
2015-09-19 21:19:22 +0200

    fix: lint -> add props validation

--- src/client/components/Root.js
+++ src/client/components/Root.js
@@ -1,10 +1,14 @@
-import React, {Component} from 'react';
+import React, {Component, PropTypes} from 'react';
 import {Provider} from 'react-redux';
 import setStore from '../../shared/setStore';
 
 const store = setStore();
 
 export default class Root extends Component {
+  static propTypes = {
+    children: PropTypes.element.isRequired
+  };
+
   render() {
     return (
       <Provider store={store}>
