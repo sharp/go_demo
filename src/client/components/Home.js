f99ee5915e922dcf33d0fd5bbdd5968adb9218bd
broucz <broucapierre@gmail.com>
2015-09-21 05:11:54 +0200

    connect Home -> view to stores

--- src/client/components/Home.js
+++ src/client/components/Home.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {Component, PropTypes} from 'react';
 import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
 import {actionAdd as addForm} from '../../shared/reducers/form';
@@ -10,7 +10,7 @@ import Thread from './helpers/Thread';
 const mapStateToProps =
   state => ({
     form: state.form,
-    answer: state.answer
+    feed: state.feed
   });
 
 // Pick available actions.
@@ -31,14 +31,26 @@ const mergeProps =
   });
 
 class Home extends Component {
+  static propTypes = {
+    builder: PropTypes.object.isRequired
+  };
+
   render() {
+    const {builder: {form, feed}} = this.props;
     return (
       <View>
-          <Thread type="fail" entries={[]}/>
         <Panel type="three_one-first">
+          <Thread
+            type="card"
+            options={{link: {baseUrl: 'form'}}}
+            entries={form.get('collection').toList()}/>
         </Panel>
-          <Thread type="fail" entries={[]}/>
         <Panel type="three_one-second">
+          <Thread type="feed" entries={
+            feed
+              .sortBy(n => n.get('count'))
+              .reverse()
+          }/>
         </Panel>
       </View>
     );
--- src/client/components/helpers/Thread.js
+++ src/client/components/helpers/Thread.js
@@ -1,11 +1,15 @@
 import React, {Component, PropTypes} from 'react';
+import {List} from 'immutable';
 import css from 'react-css-modules';
 import styles from '../../styles/thread.css';
+import Card from './Card';
+import Feed from './Feed';
 
 export class Thread extends Component {
   static propTypes = {
     type: PropTypes.string.isRequired,
-    entries: PropTypes.array.isRequired
+    entries: PropTypes.instanceOf(List).isRequired,
+    options: PropTypes.object
   };
   render() {
     const {type, entries = []} = this.props;
@@ -13,6 +17,19 @@ export class Thread extends Component {
       <div styleName="container">
         {entries.map(entry => {
           switch (type) {
+          case 'card':
+            return (
+              <Card
+                key={entry.get('id')}
+                entry={entry}
+                options={this.props.options} />
+            );
+          case 'feed':
+            return (
+              <Feed
+                key={entry.get('id')}
+                entry={entry} />
+            );
           default:
             return (
               null
