ac8144fe17fb490a4ff8b75fd951e1834692ccc6
broucz <broucapierre@gmail.com>
2015-09-28 11:20:23 +0200

    update: use more generic store construction

--- src/shared/setStore.js
+++ src/shared/setStore.js
@@ -1,20 +1,12 @@
 import {createStore, combineReducers, applyMiddleware} from 'redux';
-import form, {hydrate as hydrateForm} from './reducers/form';
-import answer, {hydrate as hydrateAnswer}  from './reducers/answer';
-import feed from './reducers/feed';
 
-const setStore =
-  (middlewares = {}) => {
-    const createStoreWithMiddleware =
-      applyMiddleware(...middlewares)(createStore);
+const setStore = (middlewares = [], reducers = {}) => {
+  const createStoreWithMiddleware =
+    applyMiddleware(...middlewares)(createStore);
 
-    return createStoreWithMiddleware(
-      combineReducers({form, answer, feed}),
-      {
-        form: hydrateForm(),
-        answer: hydrateAnswer()
-      }
-    );
-  };
+  return createStoreWithMiddleware(
+    combineReducers(reducers)
+  );
+};
 
 export default setStore;
